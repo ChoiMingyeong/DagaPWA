0f2aae49455aa87da5192f4b2d31a5c0d25941b3
최민경 <alsruddl1119@line.games>
2024-11-06 14:26:30 +0900

    PWA Pages 배포 테스트

---
+++ DagaPWA/wwwroot/service-worker.js
@@ -0,0 +1,22 @@
+﻿// service-worker.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                // 필요한 자산들 추가
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
